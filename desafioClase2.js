d349dfd59fe0c55911cc316cf5509589b733b431
Daniel Flores <daniel.flores.ang@gmail.com>
2024-03-31 19:44:18 -0600

    realizando proceso de testing

--- desafioClase2.js
+++ desafioClase2.js
@@ -4,28 +4,28 @@ class Product {
     this.description = description;
     this.price = price;
     this.thumbnail = thumbnail;
-    this.stock = stock;
     this.code = code;
+    this.stock = stock;
     this.id = id;
   }
 }
 //Productos de ejemplo
-const stratGuitar = new Product(
-  "SVR Strat",
-  "3 single coils pickups solid guitar",
+const itemTest1 = new Product(
+  "Item de prueba 1",
+  "item de prueba creado desde una clase",
   1200,
-  "exStrat.jpg",
-  3,
+  "item1.jpg",
   "JD*&-h7",
+  3,
   12
 );
-const teleGuitar = new Product(
-  "Richie Kotzen telecaster",
-  "2 single coils pickups solid guitar",
+const itemTest2 = new Product(
+  "Item de prueba 1",
+  "item de prueba creado desde una clase",
   1000,
-  "exTele.jpg",
-  5,
+  "item2.jpg",
   "KL9)7-/",
+  5,
   22
 );
 
@@ -45,10 +45,45 @@ class ProductManager {
   }
 }
 
-const productsManager = new ProductManager();
-console.log();
-productsManager.addProduct(teleGuitar);
-productsManager.addProduct(stratGuitar);
-console.log(productsManager.getProducts());
+// ---------- PROCESO DE TESTING ----------
 
-console.log(productsManager.getProductsById(2));
+/*  ✓ Se creará una instancia de la clase “ProductManager” */
+const productManager = new ProductManager();
+
+/*  ✓ Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío [] */
+console.log(productManager.getProducts());
+
+/*  ✓ Se llamará al método “addProduct” con los campos:
+- title: “producto prueba”
+- description:”Este es un producto prueba” - price:200,
+- thumbnail:”Sin imagen”
+- code:”abc123”,
+- stock:25    */
+productManager.addProduct({
+  title: "prueba",
+  description: " descripcieon",
+  price: 200,
+  thumbnail: "Sin imagen",
+  code: "abc123",
+  stock: 25,
+});
+
+
+
+/*  ✓ El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE  */
+
+/*  ✓ Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado  (falta ID)*/
+console.log(productManager.getProducts());
+
+/*  ✓ Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.  */
+
+/*  ✓ Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo  */
+
+
+
+// ---------- PRODUCTOS CREADOS DESDE UNA CLASE Y AGREGADOS AL MANAGER ----------
+productManager.addProduct(itemTest1);
+productManager.addProduct(itemTest2);
+
+
+
